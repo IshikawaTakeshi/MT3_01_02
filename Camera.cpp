35f06227cb016000d897245381aa585f7a02acc6
IshikawaTakeshi <K023G0065@g.neec.ac.jp>
2024-04-13 11:45:17 +0900

    プロジェクト ファイルを追加します。

---
+++ Camera.cpp
@@ -0,0 +1,26 @@
+﻿#include "Camera.h"
+#include "MyMath/MatrixMath.h"
+#include <imgui.h>
+
+Camera::Camera() {
+	cameraTranslate_ = { 0.0f,1.9f,-6.49f };
+	cameraRotate_ = { 0.26f,0.0f,0.0f };
+	worldMatrix_ = MatrixMath::MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate_, cameraTranslate_);
+	viewMatrix_ = MatrixMath::Inverse(worldMatrix_);
+}
+
+Camera::~Camera() {
+}
+
+void Camera::Update() {
+	worldMatrix_ = MatrixMath::MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate_, cameraTranslate_);
+	viewMatrix_ = MatrixMath::Inverse(worldMatrix_);
+
+	ImGui::Begin("Window::Camera");
+	ImGui::DragFloat3("CameraTranslate",&cameraTranslate_.x, 0.01f);
+	ImGui::DragFloat3("CameraRotate",&cameraRotate_.x, 0.001f);
+	ImGui::End();
+}
+
+void Camera::Draw() {
+}
---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <Novice.h>
+#include "MyMath/MatrixMath.h"
+#include "MyMath/MyMath.h"
+#include "Grid.h"
+#include "Sphere.h"
+#include "Camera.h"
+#include <imgui.h>
+
+const char kWindowTitle[] = "イシカワタケシ_MT3_01_02";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	Grid* grid = new Grid();
+	Camera* camera = new Camera();
+	Sphere* sphere = new Sphere();
+	//射影行列
+	Matrix4x4 projectionMatrix = MatrixMath::MakePerspectiveFovMatrix(0.45f, 1280.0f / 720.0f, 0.1f, 100.0f);
+	Matrix4x4 viewProjectionMatrix = MatrixMath::Multiply(camera->GetViewMatrix(), projectionMatrix);
+	//ビューポート変換行列
+	Matrix4x4 viewportMatrix = MatrixMath::MakeVeiwportMatrix(0, 0, 1280.0f, 720.0f, 0.0f, 1.0f);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		camera->Update();
+		sphere->Update();
+
+		viewProjectionMatrix = MatrixMath::Multiply(camera->GetViewMatrix(), projectionMatrix);
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		grid->Draw(viewProjectionMatrix,viewportMatrix);
+		sphere->Draw(viewProjectionMatrix, viewportMatrix,0xff0000ff);
+		
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
